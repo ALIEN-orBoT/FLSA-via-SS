e4023914aeca59720ef4298e10ccecb5b099f212
ALIEN-orBoT <alien_not@163.com>
2024-04-28 16:10:09 +0800

    client bit_sum done.

--- client.cxx
+++ client.cxx
@@ -24,6 +24,19 @@ uint32_t num_bits;
 
 int sockfd0, sockfd1, sockfd2;
 
+std::string pub_key_to_hex(const uint64_t* const key) {
+    std::stringstream ss;
+    ss << std::setfill('0') << std::setw(16) << std::hex << key[0];
+    ss << std::setfill('0') << std::setw(16) << std::hex << key[1];
+    return ss.str();
+}
+
+std::string make_pk(emp::PRG prg) {
+    emp::block b;
+    prg.random_block(&b, 1);
+    return pub_key_to_hex((uint64_t*)&b);
+}
+
 int send_to_server(const int server, const void* const buffer, const size_t n, const int flags = 0) {
 	const int socket = (server == 0) ? sockfd0 : ((server == 1) ? sockfd1 : sockfd2);
 //    const int socket = (server == 0 ? sockfd0 : sockfd1);
@@ -32,10 +45,79 @@ int send_to_server(const int server, const void* const buffer, const size_t n, c
     return ret;
 }
 
-void bit_sum(const std::string protocol, const size_t numreqs) {
-	std::cout << "test: use protocol:" << protocol << std::endl;
-	std::cout << "test: numreqs:" << numreqs << std::endl;
+// TODO
+int bit_sum_helper(const std::string protocol, const size_t numreqs, unsigned int &ans, const initMsg* const msg_ptr = nullptr) {
+    auto start = clock_start();
+    int num_bytes = 0;
 
+    bool real_val, share0, share1, share2;
+
+    // Can't use a fixed key, or serial will have the same key every time
+    emp::PRG prg;
+
+    BitShare* const bitshare0 = new BitShare[numreqs];
+    BitShare* const bitshare1 = new BitShare[numreqs];
+    BitShare* const bitshare2 = new BitShare[numreqs];
+    for (unsigned int i = 0; i < numreqs; i++) {
+        prg.random_bool(&real_val, 1);
+        prg.random_bool(&share0, 1);
+        prg.random_bool(&share1, 1);
+        share2 = share1 ^ share0 ^ real_val;
+        ans += real_val;
+
+        const std::string pk_s = make_pk(prg);
+        const char* const pk = pk_s.c_str();
+
+		std::cout << "TEST+++++++++++++++" << std::endl;
+        std::cout << pk << ": " <<  real_val<< " = " << share0 << " ^ " << share1<< " ^ " << share2 << std::endl;
+		std::cout << "TEST+++++++++++++++" << std::endl;
+
+        memcpy(bitshare0[i].pk, &pk[0], PK_LENGTH);
+        bitshare0[i].val = share0;
+
+        memcpy(bitshare1[i].pk, &pk[0], PK_LENGTH);
+        bitshare1[i].val = share1;
+
+        memcpy(bitshare2[i].pk, &pk[0], PK_LENGTH);
+        bitshare1[i].val = share1;
+    }
+
+    if (numreqs > 1)
+        std::cout << "batch make:\t" << sec_from(start) << std::endl;
+
+    start = clock_start();
+    if (msg_ptr != nullptr) {
+        num_bytes += send_to_server(0, msg_ptr, sizeof(initMsg));
+        num_bytes += send_to_server(1, msg_ptr, sizeof(initMsg));
+        num_bytes += send_to_server(2, msg_ptr, sizeof(initMsg));
+    }
+    for (unsigned int i = 0; i < numreqs; i++) {
+        num_bytes += send_to_server(0, &bitshare0[i], sizeof(BitShare));
+        num_bytes += send_to_server(1, &bitshare1[i], sizeof(BitShare));
+        num_bytes += send_to_server(2, &bitshare1[i], sizeof(BitShare));
+    }
+
+    delete[] bitshare0;
+    delete[] bitshare1;
+    delete[] bitshare2;
+
+    if (numreqs > 1)
+        std::cout << "batch send:\t" << sec_from(start) << std::endl;
+
+    return num_bytes;
+}
+
+void bit_sum(const std::string protocol, const size_t numreqs) {
+	unsigned int ans = 0;
+	int num_bytes = 0;
+	initMsg msg;
+	msg.num_of_inputs = numreqs;
+	msg.type = INT_SUM;
+
+	num_bytes += bit_sum_helper(protocol, numreqs, ans, &msg);
+
+	std::cout << "Ans: " << ans << std::endl;
+	std::cout << "Total sent bytes: " << num_bytes << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -119,7 +201,7 @@ int main(int argc, char** argv) {
 
 	auto start = clock_start();
 	if (protocol == "BITSUM") {
-    	msg.type = BIT_SUM;
+		std::cout << "Uploading all BITSUM shares: " << numreqs << std::endl;
 
 		// TODO
 		bit_sum(protocol, numreqs);
