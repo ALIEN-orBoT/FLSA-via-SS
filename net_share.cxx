396b14a0aecf5461c83d807a2f1a4fa47dba903c
ALIEN-orBoT <alien_not@163.com>
2024-05-06 12:30:02 +0800

    net_share done.

--- net_share.cxx
+++ net_share.cxx
@@ -32,6 +32,38 @@ int recv_in(const int sockfd, void* const buf, const size_t len) {
     return bytes_read;
 }
 
+
+int send_bool_batch(const int sockfd, const bool* const x, const size_t n) {
+    const size_t len = (n+7) / 8;  // Number of bytes to hold n, aka ceil(n/8)
+    char* buf = new char[len];
+
+    memset(buf, 0, sizeof(char) * len);
+
+    for (unsigned int i = 0; i < n; i++)
+        if (x[i])
+            buf[i / 8] ^= (1 << (i % 8));
+
+    int ret = send(sockfd, buf, len, 0);
+
+    delete[] buf;
+
+    return ret;
+}
+
+int recv_bool_batch(const int sockfd, bool* const x, const size_t n) {
+    const size_t len = (n+7) / 8;
+    char* buf = new char[len];
+
+    int ret = recv_in(sockfd, buf, len);
+
+    for (unsigned int i = 0; i < n; i++)
+        x[i] = (buf[i/8] & (1 << (i % 8)));
+
+    delete[] buf;
+
+    return ret;
+}
+
 int send_size(const int sockfd, const size_t x) {
     size_t x_conv = htonl(x);
     const char* data = (const char*) &x_conv;
@@ -55,3 +87,4 @@ int recv_uint64(const int sockfd, uint64_t& x) {
     x = ntohll(x);
     return ret;
 }
+
--- net_share.h
+++ net_share.h
@@ -3,9 +3,11 @@
 
 #include <string>
 
-
 size_t send_out(const int sockfd, const void* const buf, const size_t len);
 int recv_in(const int sockfd, void* const buf, const size_t len);
+
+int send_bool_batch(const int sockfd, const bool* const x, const size_t n);
+int recv_bool_batch(const int sockfd, bool* const x, const size_t n);
 
 int send_size(const int sockfd, const size_t x);
 int recv_size(const int sockfd, size_t& x);
